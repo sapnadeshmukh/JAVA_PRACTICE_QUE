f1af052711772fd1ec9e067b580d05a73e799c2e
Sapna Deshmukh <sapnad18@navgurukul.org>
2020-01-02 19:23:46 +0530

    update code

---
+++ MathClassExample.java
@@ -0,0 +1,21 @@
+package logical;
+
+public class MathClassExample {
+
+	public static void main(String[] args) {
+		int x=8;
+		int y=9;
+//		To find largest number from using math method
+		System.out.println("largest Number is:"  + Math.max(x, y));
+		
+		
+//		To find square root of number using math method
+		System.out.println("square root of y :" + Math.sqrt(y));
+		
+		
+//		To find smallest number using math method
+		System.out.println("smallest Number is:" + Math.min(x,y));
+
+	}
+
+}
